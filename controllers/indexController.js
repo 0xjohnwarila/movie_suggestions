add093df1fc156c61047be045bc09324f5044664
JohnNWarila <johnwarila@gmail.com>
2019-03-11 17:11:52 -0700

    investigating api response

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -14,7 +14,7 @@ exports.index = function(req, res) {
       return next(err);
     } else {
       let weather = JSON.parse(body);
-      let msg = `It's ${weather.weather} in ${city}.`;
+      let msg = body;
       res.render('index', { title: 'Movie Suggestions', message: msg });
     }
   });
