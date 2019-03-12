71d5e7a84f352ab6f8265da7d494b7f7f0f16315
JohnNWarila <johnwarila@gmail.com>
2019-03-11 17:44:31 -0700

    Switching to id from name

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -6,16 +6,21 @@ exports.index = function(req, res) {
   // get user location and the weather at location in sequence using async
   const ip = req.ipInfo;
   // this API key would be obfuscated, but the access it has is limited
-  let apiKey = 'a84da6d6eef371d1706eb04d9aaf90d7';
+  let weatherKey = 'a84da6d6eef371d1706eb04d9aaf90d7';
+  let movieKey = '43f46c28';
   let city = ip.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}`;
   request(url, function(err, response, body) {
     if(err){
       return next(err);
     } else {
       let weather = JSON.parse(body);
-      let msg = `It's ${weather.weather[0].main} in ${city}.`;
-      res.render('index', { title: 'Movie Suggestions', message: msg });
+      if(weather.weather[0].id > 799 && weather.weather[0].id < 805) {
+        res.render('index', { title: 'Movie Suggestions', message: msg, code: weather.weather[0].id});
+      }
+      else {
+        res.rener('index', { title: 'Movie Suggestions', message: 'err', code: weather.weather[0].id});
+      }
     }
   });
 };
