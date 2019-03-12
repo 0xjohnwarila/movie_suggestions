439320fa6b52de7631c873699e904042d308cf6e
JohnNWarila <johnwarila@gmail.com>
2019-03-11 17:52:47 -0700

    Added codes and updates msg

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -15,12 +15,31 @@ exports.index = function(req, res) {
       return next(err);
     } else {
       let weather = JSON.parse(body);
-      if(weather.weather[0].id > 799 && weather.weather[0].id < 805) {
-        res.render('index', { title: 'Movie Suggestions', message: msg, code: weather.weather[0].id});
+      if(weather.weather[0].id > 800 && weather.weather[0].id < 805) {
+        let msg = 'Clouds';
+      }
+      else if(weather.weather[0].id > 199 && weather.weather[0].id < 233) {
+        let msg = 'Thunder';
+      }
+      else if(weather.weather[0].id > 299 && weather.weather[0].id < 322) {
+        let msg = 'Drizzle';
+      }
+      else if(weather.weather[0].id > 499 && weather.weather[0].id < 532) {
+        let msg = 'Rain';
+      }
+      else if(weather.weather[0].id > 599 && weather.weather[0].id < 623) {
+        let msg = 'Snow';
+      }
+      else if(weather.weather[0].id > 699 && weather.weather[0].id < 782) {
+        let msg = 'Atmosphere';
+      }
+      else if(weather.weather[0].id == 800) {
+        let msg = 'Clear';
       }
       else {
-        res.render('index', { title: 'Movie Suggestions', message: 'err', code: weather.weather[0].id});
+        let msg = 'err';
       }
+      res.render('index', { title: 'Movie Suggestions', message: msg, code: weather.weather[0].id});
     }
   });
 };
